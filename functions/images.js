b42c1b6679e51bb8dfd80ab1c1a659f44f6195a0
ustaadphotoali <ustaadphotoali@gmail.com>
2026-04-19 19:09:26 -0400

    Create images.js

---
+++ functions/images.js
@@ -0,0 +1,21 @@
+export async function onRequest() {
+  const cloudName = "dqntivbw8";
+  const apiKey = "YOUR_API_KEY";
+  const apiSecret = "YOUR_API_SECRET";
+
+  const auth = btoa(`${apiKey}:${apiSecret}`);
+
+  const url = `https://api.cloudinary.com/v1_1/${cloudName}/resources/image/upload?max_results=100`;
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Basic ${auth}`
+    }
+  });
+
+  const data = await response.json();
+
+  return new Response(JSON.stringify(data.resources), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
