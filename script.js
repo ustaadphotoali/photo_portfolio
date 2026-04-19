39cdc3c203e274d231d142da0e3466cd0b28fd3b
ustaadphotoali <ustaadphotoali@gmail.com>
2026-04-19 13:55:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const cloudName = "dqntivbw8";
 
-const images = [
-  "test/photo1",
-  "test/photo2"
-];
+fetch("images.json")
+  .then(res => res.json())
+  .then(images => {
+    const gallery = document.getElementById("gallery");
 
-const gallery = document.getElementById("gallery");
-
-images.forEach((id) => {
-  const img = document.createElement("img");
-  img.src = `https://res.cloudinary.com/${cloudName}/image/upload/f_auto,q_auto/${id}.jpg`;
-  gallery.appendChild(img);
-});
+    images.forEach(id => {
+      const img = document.createElement("img");
+      img.src = `https://res.cloudinary.com/${cloudName}/image/upload/f_auto,q_auto/${id}`;
+      gallery.appendChild(img);
+    });
+  });
