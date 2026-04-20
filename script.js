7ce2ce1220984b2b8f4fe0ef1675a3cb0ffc7d81
ustaadphotoali <ustaadphotoali@gmail.com>
2026-04-19 21:20:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const gallery = document.getElementById("gallery");
 fetch("/images")
   .then(res => res.json())
   .then(images => {
+    gallery.innerHTML = ""; // clear anything old
+
     images.forEach(imgData => {
       const img = document.createElement("img");
       img.src = imgData.secure_url;
+      img.loading = "lazy"; // improves performance
+
       gallery.appendChild(img);
     });
+  })
+  .catch(err => {
+    console.error("Error loading images:", err);
   });
