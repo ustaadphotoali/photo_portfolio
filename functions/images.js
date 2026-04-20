00e685d1ef37487b67b7308ae37bb9ce637664ef
ustaadphotoali <ustaadphotoali@gmail.com>
2026-04-19 21:15:41 -0400

    Update images.js

--- functions/images.js
+++ functions/images.js
@@ -1,13 +1,21 @@
 export async function onRequest({ env }) {
+  const cloudName = "dqntivbw8";
   const apiKey = env.CLOUDINARY_API_KEY;
   const apiSecret = env.CLOUDINARY_API_SECRET;
 
-  return new Response(
-    JSON.stringify({
-      apiKeyExists: !!apiKey,
-      apiSecretExists: !!apiSecret,
-      apiKeyPreview: apiKey ? apiKey.slice(0, 4) : null
-    }),
-    { headers: { "Content-Type": "application/json" } }
-  );
+  const auth = btoa(`${apiKey}:${apiSecret}`);
+
+  const url = `https://api.cloudinary.com/v1_1/${cloudName}/resources/image/upload?max_results=100`;
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Basic ${auth}`
+    }
+  });
+
+  const data = await response.json();
+
+  return new Response(JSON.stringify(data.resources), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
